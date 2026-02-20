ddf85619e8105c051b4d2ed14f466a8070aeab5b
Jossy Quinde <jqquindem@gmail.com>
2026-02-20 16:16:57 -0500

    upload: se agrega el primer proyecto de django

---
+++ mi_web/src/proyecto/base/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from base.models import Tarea
+
+
+# Create your views here.
+class ListaPendientes(ListView):
+    model = Tarea
+    context_object_name = 'tareas'
+
+class DetalleTarea(DetailView):
+    model = Tarea
+    context_object_name = 'tarea'
+    template_name = 'base/tarea.html'
